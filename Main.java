415a23d345485e83e68bd48a8cb3255eced0f0df
Millat0422 <kawcharibenamujibmillat@gmail.com>
2025-11-08 23:12:31 +0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,60 +1,37 @@
 
-package shape;
+package animal;
 
-abstract class Shape {
 
-    abstract double calculateArea();
-    abstract double calculatePerimeter();
+
+abstract class Animal {
+  
+    abstract void sound();
 }
 
-class Circle extends Shape {
-    private double radius;
-
-    public Circle(double radius) {
-        this.radius = radius;
-    }
-
-    double calculateArea() {
-        return Math.PI * radius * radius;
-    }
-
-
-    double calculatePerimeter() {
-        return 2 * Math.PI * radius;
+class Lion extends Animal {
+   
+    void sound() {
+        System.out.println("Lion roars");
     }
 }
 
 
-class Triangle extends Shape {
-    private double side1, side2, side3;
-
-
-    public Triangle(double side1, double side2, double side3) {
-        this.side1 = side1;
-        this.side2 = side2;
-        this.side3 = side3;
-    }
-
-    double calculateArea() {
-        double s = (side1 + side2 + side3) / 2;
-        return Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
-    }
-
-    double calculatePerimeter() {
-        return side1 + side2 + side3;
+class Tiger extends Animal {
+    
+    void sound() {
+        System.out.println("Tiger growls");
     }
 }
 
 
 public class Main {
     public static void main(String[] args) {
+        
+        Animal lion = new Lion();
+        Animal tiger = new Tiger();
 
-        Shape circle = new Circle(5);
-        System.out.println("Circle Area: " + circle.calculateArea());
-        System.out.println("Circle Perimeter: " + circle.calculatePerimeter());
-
-        Shape triangle = new Triangle(3, 4, 5);
-        System.out.println("\nTriangle Area: " + triangle.calculateArea());
-        System.out.println("Triangle Perimeter: " + triangle.calculatePerimeter());
+        
+        lion.sound();
+        tiger.sound();
     }
 }
