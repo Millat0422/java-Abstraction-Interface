c2d4722f5e821e59b25b9adb38909c42c6082163
Millat0422 <kawcharibenamujibmillat@gmail.com>
2025-11-08 23:06:02 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,37 @@
+
+package animal;
+
+
+
+abstract class Animal {
+  
+    abstract void sound();
+}
+
+class Lion extends Animal {
+   
+    void sound() {
+        System.out.println("Lion roars!");
+    }
+}
+
+
+class Tiger extends Animal {
+    
+    void sound() {
+        System.out.println("Tiger growls!");
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Animal lion = new Lion();
+        Animal tiger = new Tiger();
+
+        
+        lion.sound();
+        tiger.sound();
+    }
+}
